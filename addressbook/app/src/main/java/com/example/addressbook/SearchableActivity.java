6048fb7002c8024ca7ba125358756c7353edaa2e
Neena Ugail <nhu21@bath.ac.uk>
2020-09-14 23:02:26 +0100

    began coding search bar

---
+++ addressbook/app/src/main/java/com/example/addressbook/SearchableActivity.java
@@ -0,0 +1,23 @@
+package com.example.addressbook;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchable);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+}
